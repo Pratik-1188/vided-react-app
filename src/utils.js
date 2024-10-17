8c179ce6d823c18b6ce55ce9349f8c9bc15dc4cf
Pratik Bhosale <pratikbhosale1188@gmail.com>
2024-10-17 11:43:21 +0530

    added cloud backend endpoint

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,9 @@
 export const getdomain = () => {
-    if(window.location.host.includes("127.0.0.1") || window.location.host.includes("localhost"))
-        return "http://localhost:8080"
-    else
-        return ""
-}
+  if (
+    window.location.host.includes("127.0.0.1") ||
+    window.location.host.includes("localhost")
+  )
+    return "http://localhost:8080";
+  else
+    return "https://vided-spring-boot-app.internal.happysand-0cd6e089.centralindia.azurecontainerapps.io";
+};
